1e8b9d3097b2496b5157b0a29c116eafddd0cd60
ilhamhadisyah <ilhamhadisyahsmd@gmail.com>
2021-04-06 01:55:50 +0700

    add splash screen

---
+++ app/src/main/java/com/ilham/githubmemberapp/ui/view/splash/Splash.kt
@@ -0,0 +1,29 @@
+package com.ilham.githubmemberapp.ui.view.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ilham.githubmemberapp.databinding.ActivitySplashBinding
+import com.ilham.githubmemberapp.ui.view.main.MainActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class Splash : AppCompatActivity() {
+    private lateinit var binding : ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        GlobalScope.launch {
+            delay(2000L)
+            moveIntent()
+        }
+    }
+    private fun moveIntent(){
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
